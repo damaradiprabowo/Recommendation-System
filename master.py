ead3808c8ce39d172a09b1a22c7187cc38581082
Damar Adi <damaradiprabowo@gmail.com>
2019-09-21 00:01:15 +0700

    Update dataset & master

--- master.py
+++ master.py
@@ -8,46 +8,45 @@ Created on Fri Sep 20 22:20:27 2019
 import pandas as pd
 import sys
 import os
+import json
 
 os.chdir('models')
 dir = os.getcwd()
 sys.path.insert(0, dir)
 
-from apriori import Apriori
-from collaborative import Collaborative
-from content import ContentBased
-from hybrid import Hybrid
-from abtest import ABTesting
+##from apriori import Apriori
+##from collaborative import Collaborative
+##from content import ContentBased
+#from hybrid import Hybrid
+##from abtest import ABTesting
 
-#change dir to main folder for geting a dataset in Dataset directory
-os.chdir('../dataset')#change dir to Dataset folder for geting a dataset in that directory
+
+os.chdir('../dataset')
 dir = os.getcwd()
 sys.path.insert(0, dir)
 
-df_apriori = pd.read_csv('apriori_dataset.csv')
+df_apriori = pd.read_csv('apriori_dataset.csv',sep=';')
+with open('collaborative_dataset.json', 'r') as f:
+    df_collab = json.load(f)
+df_content = pd.read_csv('content_dataset.csv')
+df_hybrid = pd.read_csv('hybrid_dataset.csv',sep=';')
 
-
-print('+ Association with apriori: ')
-apr = Apriori('Watch')
+print('Association rule with apriori: ')
+apr = Apriori(support=0.1,  confidence=0.1)
 apr.main(df_apriori)
 apr.result()
 
-##Content based
-#print('+ Recommendation by content based Avenger: ')
-#content = ContentBased(df_content)
-#print(content.predict('Avenger'))
-#
-#print('=====================================')
-##Collaborative
-#print('+ Collaborative Filtering: ')
-#julcollab = JulCollaborative(dataset)
-#recommendation, score = julcollab.user_recommendations('Dpv')
-#print('user recommendation for Dpv with score: ', score)
-#print('user correlation score between Dpv and Star: ', julcollab.person_correlation('Dpv', 'Star'))
-#print('users who have similarities with Dpv in genre film: ', julcollab.most_similar_users('Dpv',2))
-#
-#print('=====================================')
-##Hybrid
-#print('+ Recommendation user jul with hybrid algorithm (minimumscore = 0.1):')
-#julhybrid = JulHybrid(film=df_hybrid2, dataset=dataset)
-#print(julhybrid.predict())
+print('Collaborative Filtering: ')
+collab = Collaborative(df_collab)
+recommendation, score = collab.user_recommendations('ANI')
+print('user recommendation for ANI with score: ', score)
+print('user correlation score between ANI and Dpv: ', collab.person_correlation('ANI', 'Dpv'))
+print('users who have similarities with ANI in genre film: ', collab.most_similar_users('ANI',1))
+
+print('Recommendation with content based: ')
+content = ContentBased(df_content)
+print(content.predict('Avenger'))
+
+print('Recommendation for ANI with hybrid algorithm:')
+hybrid = Hybrid(film=df_hybrid, dataset=df_collab)
+print(hybrid.predict())
